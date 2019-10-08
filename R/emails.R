b154a28f8a973b361c6da31de27866337fbf8764
Adrienne Stilp <amstilp@uw.edu>
2019-10-08 16:49:06 -0700

    Export the print_emails function
    Add documentation for the print_emails function. As part of this,
    combine the documentation with the get_emails function since they
    have essentially the same purpose with a slightly different return
    value.

--- R/emails.R
+++ R/emails.R
@@ -12,27 +12,44 @@
     stop("tbl must have a column 'email'")
   }
 }
+
+
 #' Obtain emails in the table
+#' 
+#' These functions collapse the emails in the provided \code{tbl} to
+#' a comma-separated string that can be cut and paste into an email
+#' client's "To:" field.
 #' 
 #' @param tbl A tibble with one required column "email"
 #' @param sorted Should the emails be sorted before returning?
 #' 
 #' @seealso parse_contact_table
 #' 
-#' @export
+#' @name emails
 #' 
-#' @examples{
-#' 
+#' @examples
 #' html_file <- system.file(package = "contactparser", "testdata", "example.html")
 #' parsed <- parse_contact_table(html_file)
+#' 
 #' get_emails(parsed)
 #' 
-#' }
+#' print_emails(parsed)
+#' 
+NULL
+
+#' @describeIn emails return an email list
+#' 
+#' @export
+#' 
 get_emails <- function(tbl, sorted = TRUE) {
   .check_email_column(tbl)
   return(.construct_email_list(tbl$email, sorted = sorted))
 }
 
+#' @describeIn emails print the emails to the console
+#' 
+#' @export
+#' 
 print_emails <- function(tbl, sorted = TRUE) {
   .check_email_column(tbl)
   message(.construct_email_list(tbl$email, sorted = sorted))
